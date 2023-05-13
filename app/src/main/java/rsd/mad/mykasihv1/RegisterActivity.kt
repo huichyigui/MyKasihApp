a4fe07238a259d549a32cbcee445a5ed83ef3e5d
hq <109281026+chuustagram@users.noreply.github.com>
2023-05-13 08:44:14 +0800

    Update RegisterActivity.kt

--- app/src/main/java/rsd/mad/mykasihv1/RegisterActivity.kt
+++ app/src/main/java/rsd/mad/mykasihv1/RegisterActivity.kt
@@ -1,15 +1,10 @@
 package rsd.mad.mykasihv1
 
-import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
-import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -17,13 +12,11 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import rsd.mad.mykasihv1.databinding.ActivityRegisterBinding
-import rsd.mad.mykasihv1.models.User
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var auth : FirebaseAuth
-    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +25,8 @@ class RegisterActivity : AppCompatActivity() {
         supportActionBar?.title = getString(R.string.registration)
 
         auth = Firebase.auth
-        progressDialog = ProgressDialog(this)
-        progressDialog.setTitle("Please wait")
-        progressDialog.setCanceledOnTouchOutside(false)
 
-        binding.btnRegister.setOnClickListener { register() }
+        binding.btnNext.setOnClickListener { register2() }
         binding.hplLogin.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
@@ -47,13 +37,11 @@ class RegisterActivity : AppCompatActivity() {
     private var mobile = ""
     private var password = ""
     private var role = ""
-    private var house = 0
-    private var street = ""
-    private var city = ""
 
-    private fun register() {
+    private fun register2() {
         // Validate Data
         var isValid = true
+        val intent = Intent(this, Register2Activity::class.java)
 
         with(binding) {
             name = edtNameRegister.text.toString().trim()
@@ -61,9 +49,6 @@ class RegisterActivity : AppCompatActivity() {
             mobile = edtMobileRegister.text.toString().trim()
             password = edtPasswordRegister.text.toString()
             role = if (rbDonor.isChecked) "Donor" else "Donee"
-            house = edtHouseRegister.text.toString().toIntOrNull() ?: 0
-            street = edtStreetRegister.text.toString().trim()
-            city = edtCityRegister.text.toString().trim()
 
             if (name.isEmpty()) {
                 edtNameRegister.error = getString(R.string.err_empty)
@@ -78,82 +63,27 @@ class RegisterActivity : AppCompatActivity() {
                 isValid = false
             }
 
-            val phoneRegex = Regex("^01\\d-\\d{7,8}$")
             if (mobile.isEmpty()) {
                 edtMobileRegister.error = getString(R.string.err_empty)
                 isValid = false
-            } else if (!phoneRegex.matches(mobile)) {
+            } else if (!Patterns.PHONE.matcher(mobile).matches()) {
                 edtMobileRegister.error = getString(R.string.err_mobile)
                 isValid = false
             }
 
-            val passwordRegex = Regex(".{6,}")
             if (password.isEmpty()) {
                 edtPasswordRegister.error = getString(R.string.err_empty)
-                isValid = false
-            } else if (!passwordRegex.matches(password)) {
-                edtPasswordRegister.error = getString(R.string.err_password)
-                isValid = false
-            }
-
-            if (house <= 0) {
-                edtHouseRegister.error = getString(R.string.err_house)
-                isValid = false
-            }
-
-            if (street.isEmpty()) {
-                edtStreetRegister.error = getString(R.string.err_empty)
-                isValid = false
-            }
-
-            if (city.isEmpty()) {
-                edtCityRegister.error = getString(R.string.err_empty)
                 isValid = false
             }
 
             if (isValid) {
-                progressDialog.setMessage("Creating account...")
-                progressDialog.show()
+                intent.putExtra(getString(R.string.name), name)
+                intent.putExtra(getString(R.string.email), email)
+                intent.putExtra(getString(R.string.mobile), mobile)
+                intent.putExtra(getString(R.string.password), password)
+                intent.putExtra(getString(R.string.role), role)
 
-                val address = "$house $street"
-
-                progressDialog.setMessage("Saving user info")
-
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val user: User = User(name, email, mobile, city, address)
-                        val database = Firebase.database
-
-                        auth.currentUser?.let { it1 ->
-                            database.getReference("Users").child(role).child(it1.uid)
-                                .setValue(user)
-                                .addOnSuccessListener {
-                                    progressDialog.dismiss()
-                                    toast("User registered")
-                                    var i = Intent(this@RegisterActivity, MainActivity::class.java
-                                    )
-                                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                                    startActivity(i)
-                                    finish()
-                                }
-                                .addOnFailureListener { e ->
-                                    progressDialog.dismiss()
-                                    toast("Database error")
-                                }
-                        }
-                    } else {
-                        progressDialog.dismiss()
-                        when (it.exception) {
-                            is FirebaseAuthWeakPasswordException -> {
-                                toast("Password should be at least of 6 characters")
-                                edtPasswordRegister.error = "Password should be at least of 6 characters"
-                            }
-                            is FirebaseAuthUserCollisionException -> toast("This email is already in use")
-                            is FirebaseAuthInvalidCredentialsException -> toast("Please enter a valid email")
-                            else -> toast("Error")
-                        }
-                    }
-                }
+                startActivity(intent)
             }
         }
     }
@@ -191,13 +121,10 @@ class RegisterActivity : AppCompatActivity() {
                 }
 
                 override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
                 }
 
             })
         }
-    }
-
-    private fun toast(s: String) {
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
     }
 }
