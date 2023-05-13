e566125eb7199025bfe36233731adfaa8d4553cd
hq <109281026+chuustagram@users.noreply.github.com>
2023-05-13 19:20:31 +0800

    Add proof image in ClaimDetailFragment.kt

--- app/src/main/java/rsd/mad/mykasihv1/ui/history/ClaimDetailFragment.kt
+++ app/src/main/java/rsd/mad/mykasihv1/ui/history/ClaimDetailFragment.kt
@@ -1,14 +1,18 @@
 package rsd.mad.mykasihv1.ui.history
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.navigation.fragment.NavHostFragment
+import com.squareup.picasso.Picasso
 import rsd.mad.mykasihv1.Helper
+import rsd.mad.mykasihv1.Helper.Companion.toPx
 import rsd.mad.mykasihv1.R
 import rsd.mad.mykasihv1.databinding.FragmentClaimDetailBinding
 import rsd.mad.mykasihv1.models.Donation
@@ -76,6 +80,21 @@ class ClaimDetailFragment : Fragment() {
 
                 tblPickupInfo.addView(tableRow)
             }
+            val rowProof = TableRow(context)
+            val colProof1 = TextView(context)
+            colProof1.text = "Proof Image"
+            rowProof.addView(colProof1)
+
+            val rowProof2 = TableRow(context)
+            val colProof2 = ImageView(context)
+            val layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 2f)
+            layoutParams.height =  200.toPx(requireContext())
+            colProof2.layoutParams = layoutParams
+            colProof2.scaleType = ImageView.ScaleType.CENTER_CROP
+            Picasso.with(context).load(donor!!.proofImage).placeholder(R.drawable.progress_animation).error(R.drawable.try_later).into(colProof2)
+            rowProof2.addView(colProof2)
+            tblPickupInfo.addView(rowProof)
+            tblPickupInfo.addView(rowProof2)
         }
     }
 }
